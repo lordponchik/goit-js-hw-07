53728f9ede11dc77581467247bec00e30f9be152
Denis Strashnyuk <strashnyuk.denis@gmail.com>
2022-02-05 14:16:07 +0200

    Рендер разметки

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,25 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const galleryEl = document.querySelector('.gallery');
+
+galleryEl.insertAdjacentHTML('beforeend', makeRenderGallery(galleryItems));
+
+function makeRenderGallery(gallery) {
+  return gallery
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+    })
+    .join('');
+}
+
 console.log(galleryItems);
