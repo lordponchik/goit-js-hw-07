2ba1cfc00ab35643c50e06a4976b2839e682ca3a
Denys Strashniuk <denysstrashniuk@MacBook-Pro-Denys-Strashniuk.local>
2023-04-25 09:35:10 +0200

    add task-02

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,23 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+
+galleryEl.innerHTML = renderGallery(galleryItems);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+function renderGallery(gallery) {
+  return gallery
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+                 <a class="gallery__link" href="${original}">
+                   <img class="gallery__image" src="${preview}" alt="${description}" />
+               </a>
+            </li>`;
+    })
+    .join('');
+}
