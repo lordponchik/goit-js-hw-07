5768a13d55d2d147a46943263da6c7a53839a35a
Denys Strashniuk <denysstrashniuk@MacBook-Pro-Denys-Strashniuk.local>
2023-04-25 09:04:04 +0200

    add task-01

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,51 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEL = document.querySelector('.gallery');
+
+galleryEL.innerHTML = renderGallery(galleryItems);
+
+galleryEL.addEventListener('click', e => {
+  e.preventDefault();
+
+  if (!e.target.classList.contains('gallery__image')) {
+    return;
+  }
+
+  const instance = basicLightbox.create(
+    `<img src="${e.target.dataset.source}" alt="${e.target.alt}">`,
+    {
+      onShow(instance) {
+        window.addEventListener('keydown', onEscapeCloseModal);
+      },
+      onClose(instance) {
+        window.removeEventListener('keydown', onEscapeCloseModal);
+      },
+    }
+  );
+
+  instance.show();
+
+  function onEscapeCloseModal(e) {
+    if (e.key === 'Escape') {
+      instance.close();
+    }
+  }
+});
+
+function renderGallery(gallery) {
+  return gallery
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`;
+    })
+    .join('');
+}
